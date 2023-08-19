013a99faa618660815fbd481e126bac03533da48
Nathan <feithlicher@gmail.com>
2023-08-19 17:58:40 +0300

    added map class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 // import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 // const user = new User();
 // console.log(user);
@@ -9,12 +10,6 @@
 // const company = new Company();
 // console.log(company);
 
+const customMap = new CustomMap('map');
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
 
